a071a4101d9d827df40440e571932368e6010f84
Rei Lin <temisrei@gmail.com>
2017-08-01 14:24:41 +0800

    LifeCycle 比較 constructor, ngOnInit, onOnChanges

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,21 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   @Input('srvElement') element: {name: string, type: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called');
   }
 
 }
